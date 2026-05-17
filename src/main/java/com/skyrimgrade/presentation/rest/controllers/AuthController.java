ca1aed9fdcfb03b27b5804aa856c9b18273ea0bb
Abay Kinayat <golemplay.gp@gmail.com>
2026-05-17 20:38:31 +0500

    test: тестирование

--- src/main/java/com/skyrimgrade/presentation/rest/controllers/AuthController.java
+++ src/main/java/com/skyrimgrade/presentation/rest/controllers/AuthController.java
@@ -1,17 +1,35 @@
 package com.skyrimgrade.presentation.rest.controllers;
 
+import com.skyrimgrade.infrastructure.http.HttpContext;
 import com.skyrimgrade.infrastructure.http.annotations.Controller;
 import com.skyrimgrade.infrastructure.http.annotations.Post;
+import com.skyrimgrade.shared.validation.annotation.annotations.Email;
+import com.skyrimgrade.shared.validation.annotation.annotations.MaxLength;
+import com.skyrimgrade.shared.validation.annotation.annotations.MinLength;
+import com.skyrimgrade.shared.validation.annotation.annotations.Required;
 
 @Controller("/auth")
 public class AuthController {
-  
+
+  record AuthenticateRequest(
+      @Required
+      @Email
+      String email,
+
+      @Required
+      @MinLength(length = 6)
+      @MaxLength(length = 64)
+      String password
+  ) {}
+
   @Post("/authenticate")
-  public void authenticate() {
+  public void authenticate(HttpContext ctx) throws Exception {
+    AuthenticateRequest body = ctx.body(AuthenticateRequest.class);
+    ctx.json(200, body);
   }
 
   @Post("/registration")
-  public void registration() {
+  public void registration(HttpContext ctx) {
   }
-  
+
 }
--- src/main/java/com/skyrimgrade/presentation/rest/controllers/UserContextController.java
+++ src/main/java/com/skyrimgrade/presentation/rest/controllers/UserContextController.java
@@ -1,13 +1,14 @@
 package com.skyrimgrade.presentation.rest.controllers;
 
+import com.skyrimgrade.infrastructure.http.HttpContext;
 import com.skyrimgrade.infrastructure.http.annotations.Controller;
 import com.skyrimgrade.infrastructure.http.annotations.Get;
 
 @Controller("/user-context")
 public class UserContextController {
-  
+
   @Get("/")
-  public void getUserContext() {
+  public void getUserContext(HttpContext ctx) {
 
   }
 }
